b75630d1175ced0cd7070a11cc04454826a532a7
Xen0Xys <red57101@gmail.com>
2024-11-07 09:58:47 +0100

    :sparkles: Implement quiz creation

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,9 +1,10 @@
 import {Module} from "@nestjs/common";
 import {QuizController} from "./quiz.controller";
 import {QuizService} from "./quiz.service";
+import {ServicesModule} from "../../common/services/services.module";
 
 @Module({
-    imports: [],
+    imports: [ServicesModule],
     exports: [],
     controllers: [QuizController],
     providers: [QuizService],
--- src/modules/quiz/quiz.service.ts
+++ src/modules/quiz/quiz.service.ts
@@ -1,15 +1,64 @@
 import {Injectable, InternalServerErrorException, NotFoundException} from "@nestjs/common";
 import {CategoryEntity} from "./models/entities/category.entity";
 import {DifficultyEntity} from "./models/entities/difficulty.entity";
+import {PrismaService} from "../../common/services/prisma.service";
+import {CipherService} from "../../common/services/cipher.service";
+import * as he from "he";
 
 @Injectable()
 export class QuizService{
     constructor(
-
+        private readonly prismaService: PrismaService,
+        private readonly cipherService: CipherService,
     ){}
 
+    private generateQuizCode(): string{
+        return Math.random().toString(36).substring(2, 8).toUpperCase();
+    }
+
+    private findCategoryFromName(name: string, categories: CategoryEntity[]): CategoryEntity{
+        return categories.find(category => category.name.toLowerCase() === he.decode(name).toLowerCase());
+    }
+
+    private findDifficultyFromName(name: string, difficulties: DifficultyEntity[]): DifficultyEntity{
+        return difficulties.find(difficulty => difficulty.name.toLowerCase() === name.toLowerCase());
+    }
+
     async createQuiz(questionCount: number, categoryId?: number, difficultyId?: number){
-        return await this.getQuestions(questionCount, categoryId, difficultyId);
+        const questions = await this.getQuestions(questionCount, categoryId, difficultyId);
+        const code = this.generateQuizCode();
+        const categories = await this.getCategories();
+        const difficulties = this.getDifficulties();
+        const dbQuestions = await Promise.all(questions.map(async(question: any) => {
+            return {
+                id: this.cipherService.getSum(question.question),
+                question: he.decode(question.question),
+                correct_answer: he.decode(question.correct_answer),
+                incorrect_answers: question.incorrect_answers.map((answer: string) => he.decode(answer)),
+                category: this.findCategoryFromName(question.category, categories).id,
+                difficulty: this.findDifficultyFromName(question.difficulty, difficulties).id,
+            };
+        }));
+        await this.prismaService.questions.createMany({
+            data: dbQuestions,
+            skipDuplicates: true
+        });
+        await this.prismaService.quiz.create({
+            data: {
+                code,
+                category: categoryId,
+                difficulty: difficultyId,
+            }
+        });
+        const quizQuestions = dbQuestions.map((question: any) => {
+            return {
+                quiz_code: code,
+                question_id: question.id,
+            };
+        });
+        await this.prismaService.quizQuestions.createMany({
+            data: quizQuestions
+        });
     }
 
     private async getQuestions(questionCount: number, categoryId?: number, difficultyId?: number){
