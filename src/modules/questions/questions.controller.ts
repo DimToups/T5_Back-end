ea405d40881c4d384c0317d7159ea6ca37188dc4
Xen0Xys <red57101@gmail.com>
2024-11-20 10:20:15 +0100

    :memo: Update documentation

--- src/modules/questions/questions.controller.ts
+++ src/modules/questions/questions.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post, Query} from "@nestjs/common";
+import {Body, Controller, Get, HttpCode, HttpStatus, Post, Query} from "@nestjs/common";
 import {ApiTags} from "@nestjs/swagger";
 import {QuestionsService} from "./questions.service";
 import {QuestionEntity} from "./models/entities/question.entity";
@@ -11,11 +11,22 @@ export class QuestionsController{
         private readonly questionsService: QuestionsService,
     ){}
 
+    /**
+     * Generate questions from open trivia database
+     *
+     * @throws {500} Internal Server Error
+     */
     @Post("generate")
+    @HttpCode(HttpStatus.OK)
     async generateQuestions(@Body() body: GenerateQuestionDto): Promise<QuestionEntity[]>{
         return this.questionsService.generateQuestions(body.amount, body.difficulty, body.category);
     }
 
+    /**
+     * Get questions from database
+     *
+     * @throws {500} Internal Server Error
+     */
     @Get()
     async getQuestions(@Query() query: GenerateQuestionDto): Promise<QuestionEntity[]>{
         return this.questionsService.getQuestions(query.amount, query.difficulty, query.category);
